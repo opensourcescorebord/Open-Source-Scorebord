e98542dd214378a27eb4c1a01856ee5054c479e5
opensourcescorebord <40198767+opensourcescorebord@users.noreply.github.com>
2018-06-15 13:27:45 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<html lang="en">
+<head>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+
+</head>
+
+<body>
+ 
+
+<h2 class="text-center font-weight-bold text-black-50 p-0 m-0">Open Source Scorebord</h2>
+
+<br>
+
+ <div class="container"> 
+	<div class="row">
+		<div class="col-sm">
+		</div>
+		<div class="col-sm"> 
+			<form class="form-signin">
+				<a href="/Toeschouwer/Waitingroom.php" class="btn btn-lg btn-block btn-info" role="button">Toeschouwer</a>
+				<a href="/Bestuurder/Login.php" class="btn btn-lg btn-block btn-info" role="button">Bestuurder</a>
+			</form>
+		</div>
+		<div class="col-sm">
+		</div>	
+	
+</div>
+</div>
+ 
+
+</body>
