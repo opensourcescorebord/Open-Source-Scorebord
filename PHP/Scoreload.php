1f13968708ac12bf534a639dea01f63882110e45
SNGomashie <jhdensen@gmail.com>
2018-06-17 13:26:03 +0200

    mweh

--- PHP/Scoreload.php
+++ PHP/Scoreload.php
@@ -1,16 +1,23 @@
 <?php
-define('DOCUMENT_ROOT',dirname(__FILE__));
-include(DOCUMENT_ROOT.'/ConDB.php');
-session_start();
-$Game_ID = $_COOKIE['code'];
+include '../PHP/ConDB.php';
+if (!isset($_COOKIE['code'])) {
+  $Game_ID = $_SESSION['code'];
+} else {
+  $Game_ID = $_GET['code'];
+}
+
 
 $result = $dbC->query("SELECT Timeout, Team_1, Team_2, Extended_Time, Extended, Score_1, Score_2 FROM games WHERE Game_ID = '$Game_ID'");
 if ($result->num_rows > 0) {
  while ($row = $result->fetch_assoc()) {
 $score['Sc'] = $row;
 echo json_encode($score);
+} else {
+  echo "Error no results"
 }
 
+} else {
+  echo "Error no rows"
 }
 
  ?>
--- PI/PIwaitingroom.php
+++ PI/PIwaitingroom.php
@@ -13,17 +13,38 @@
    <head>
      <meta charset="utf-8">
      <meta name="viewport" content="width=device-width, initial-scale=1">
+     <link rel="icon" href="http://i218.photobucket.com/albums/cc233/shoopuff1/Opensource%20Scorebord/HampK%20Icon%20A.png">
+     <!-- Bootstrap -->
+     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+       <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+     <link rel="icon" href="../../../../favicon.ico">
      <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
      <title>Waiting room</title>
+
+     <style media="screen">
+     body,html {
+       height: 100%;
+       background-image: url("http://i218.photobucket.com/albums/cc233/shoopuff1/Opensource%20Scorebord/CvL%20double%20Logo%2045%20degrees%20xsm.png");
+       overflow:hidden;
+     }
+     </style>
    </head>
 <body>
   <div>
-           <h1>Welcome!</h1>
-           <h2>A game will start shortly</h2>
-            <h3 id="Teams">NVT</h3>
-            <h1 id="Code"></h1>
+    <br>
+    <br>
+    <div class="row justify-content-center">
+      <div class="col-8 w-50 py-5 card shadow-lg">
+        <h1 class="text-center">Welcome!</h1>
+        <h2 class="text-center">A game will start shortly</h2>
+         <h3 id="Teams" class="text-center">NVT</h3>
+         <h1 id="Code" class="display-3 text-center">---</h1>
+      </div>
+    </div>
+
            <script type="text/javascript">
 
            var getTeams = function () {
