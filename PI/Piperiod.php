4a5f3355e93058d9ca158b09c9ef3046d3c3960a
SNGomashie <jhdensen@gmail.com>
2018-06-24 20:53:33 +0200

    ;

--- PI/Piperiod.php
+++ PI/Piperiod.php
@@ -1,6 +1,6 @@
 <?php
 include '../PHP/ConDB.php';
- $result1 = $dbC->query("SELECT Perdiod FROM games WHERE Game_ID='$_COOKIE[code]'");
+ $result1 = $dbC->query("SELECT Perdiod FROM games WHERE User='1' AND Active='1' AND End_Time IS NULL");
    if ($result1->num_rows > 0) {
     while ($row = $result1->fetch_assoc()) {
      $FK = $row['Perdiod'];
